4d05413cde6a9732838c36f73d4f098ac272d4e7
MihailShivak <mihailshivak@yandex.ru>
2025-07-26 21:50:13 +0300

    Telegram_Web_App_Erosia_26_07_25_21_50

---
+++ Telegram_Web_App/Erosia/server/index.js
@@ -0,0 +1,13 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config()
+
+
+const token = process.env.TELEGRAM_WEB_APP_TOKEN;
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Received your message');
+});
