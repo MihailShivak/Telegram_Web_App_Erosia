532c2bb8340a4c4f390a38125ce7938f92289dcb
MihailShivak <mihailshivak@yandex.ru>
2025-07-28 01:26:24 +0300

    Telegram_Web_App_Erosia_telegram-bot-branch_28_07_25_01_25

--- Telegram_Web_App/server/telegram-bot/bot.js
+++ Telegram_Web_App/server/telegram-bot/bot.js
@@ -3,10 +3,23 @@ const { TELEGRAM_WEB_APP_TOKEN, EXECUTOR_CHAT_ID, WEB_APP_URL } = require('./con
 
 const bot = new TelegramBot(TELEGRAM_WEB_APP_TOKEN, { polling: true });
 
-// Обработка команды /start /help
+// userState для обработки состояния пользователя при направлении обращения
+const userState = {}
+
+// Подсказки с доступными командами
+bot.setMyCommands([
+  { command: '/start', description: '✨ Запустить бота' },
+  { command: '/help', description: '📋 Помощь по командам' },
+  { command: '/shop', description: '🛍️ Открыть магазин' },
+  { command: '/support', description: '❓ Написать в поддержку' },
+  { command: '/stop', description: '🏁 Завершение текущего взаимодействия' }
+]);
+
+// Обработка команды /start /help /shop /support /stop
 bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id
-    bot.sendMessage(chatId, 'Текстовое сообщение', {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'Главное меню:', {
         reply_markup:{
             inline_keyboard: [
                 [
@@ -16,7 +29,10 @@ bot.onText(/\/start/, (msg) => {
                     }
                 ],
                 [
-                    { text: '❓ Помощь', callback_data: 'help' }
+                    { 
+                        text: '❓ Помощь', 
+                        callback_data: 'support' 
+                    }
                 ]
             ]
         }
@@ -24,8 +40,23 @@ bot.onText(/\/start/, (msg) => {
 });
 
 bot.onText(/\/help/, (msg) => {
-    const chatId = msg.chat.id
-    bot.sendMessage(chatId, 'Текстовое сообщение', {
+    const helpCommandText = `
+📝 *Доступные команды:*
+
+    /start -  ✨ Запустить бота
+    /help -  📋 Помощь по командам
+    /shop -  🛍️ Открыть магазин
+    /support -  ❓ Написать в поддержку
+    /stop -  🏁 Завершение текущего взаимодействия
+    `;
+
+    bot.sendMessage(msg.chat.id, helpCommandText, {parse_mode: 'Markdown'});
+});
+
+bot.onText(/\/shop/, (msg) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'Открыть магазин', {
         reply_markup:{
             inline_keyboard: [
                 [
@@ -33,12 +64,90 @@ bot.onText(/\/help/, (msg) => {
                         text: '🛍️ Магазин',
                         web_app: { url: WEB_APP_URL }
                     }
-                ],
+                ]
+            ]
+        }
+    });
+    
+});
+
+bot.onText(/\/support/, (msg) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'Получить помощь', {
+        reply_markup:{
+            inline_keyboard: [
                 [
-                    { text: '❓ Помощь', callback_data: 'help' }
+                    {
+                        text: '❓ Помощь',
+                        callback_data: 'support'
+                    }
                 ]
             ]
         }
     });
 });
 
+bot.onText(/\/stop/, (msg) => {
+    const chatId = msg.chat.id;
+
+   if (userState[chatId] === 'awaiting_support'){
+    bot.sendMessage(chatId, `
+❗ Вы вышли из режима обращения
+        `)
+   } else {
+    bot.sendMessage(chatId, `
+❗ У вас нет активного взаимодействия.
+✨ Чтобы начать - используйте /start
+        `)
+   }
+});
+
+// callback меняет состояние пользователя
+bot.on('callback_query', (query) => {
+    const chatId = query.message.chat.id;
+
+    if(query.data === 'support'){
+        bot.sendMessage(chatId, `
+❗ Обращение должно содержать следующие данные:
+
+    👤 ФИО: Иванов Иван
+    📱 Телефон: +79123456789
+    💬 Сообщение: Мне нужна Помощь !
+
+Введите текст обращения:
+            `);
+        userState[chatId] = 'awaiting_support';
+        bot.answerCallbackQuery(query.id);
+    }
+});
+
+// обработка callback'a
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    // Взять Chat ID
+    // console.log('Chat ID:', msg.chat.id);
+
+    if(text.startsWith('/')) return;
+
+    if (userState[chatId] === 'awaiting_support'){
+        const username = msg.from.username ? `@${msg.from.username}` : 'не указан';
+        const supportMessage = `
+📩 Новое обращение от пользователя:
+
+🆔 ID: ${msg.from.id}
+👤 Username: ${username}
+
+💬 Сообщение:
+${text}
+        `;
+
+        bot.sendMessage(EXECUTOR_CHAT_ID, supportMessage.trim());
+        bot.sendMessage(chatId, `
+✅ Ваше сообщение отправлено !  
+            `);
+        delete userState[chatId];
+    }
+});
